30f8209ee2656aa057aae6e5b457c2f4eef9586b
cnxct <cfc4n@cnxct.com>
2012-09-07 15:58:39 +0800

    增加当前文件MD5 hash 写入

--- apc_dump.php
+++ apc_dump.php
@@ -4,14 +4,14 @@
  **  需要保证导出服务器的web路径跟目标运行服务器的web路径一致
  **  导出：要确认 wwwroot同级目录下，存在bin目录，之后，执行此文件(放在tool目录下)，出现“Apc cache done!”即视为成功，之后将bin文件以及其他所有文件(多数已经被清空)复制到目标服务器。
  **  导入：在php.ini中新增apc.preload_binfile="/data/xyws.bin" 即可
- **  Author CFC4N cfc4n@cnxct.com $Id: apc_dump.php 1523 2012-08-15 11:52:03Z cfc4n $
+ **  AUthor CFC4N cfc4n@cnxct.com $Id: apc_dump.php 1523 2012-08-15 11:52:03Z cfc4n $
  */
 define('PROJECTROOT',substr(dirname(__FILE__), 0, -13).DIRECTORY_SEPARATOR);
 define('SYSTEMROOT',PROJECTROOT.'system'.DIRECTORY_SEPARATOR);
 define('WWWROOT',PROJECTROOT.'wwwroot'.DIRECTORY_SEPARATOR);
 define('SERVERROOT',PROJECTROOT.'server'.DIRECTORY_SEPARATOR);
 define('APCBIN',PROJECTROOT.'bin'.DIRECTORY_SEPARATOR);
-
+define('BINNAME','xyws.bin');
 //定义需要cache的目录
 $arrCacheDir = array();
 /* AMF核心模块 缓存目录*/
@@ -23,11 +23,26 @@ array_push($arrCacheDir,WWWROOT.'controllers',WWWROOT.'helpers',WWWROOT.'hooks',
 /* 框架缓存目录 */
 array_push($arrCacheDir,SYSTEMROOT.'config',SYSTEMROOT.'database',SYSTEMROOT.'errors',SYSTEMROOT.'helpers',SYSTEMROOT.'kernel',SYSTEMROOT.'libraries');
 
+
 /* 后台守护进程缓存目录 cli模式运行，务必不要缓存 脚本目录，同时在system\server.php中 加入apc_bin_loadfile APC导出的bin文件 */
-array_push($arrCacheDir,SERVERROOT.'daemon',SERVERROOT.'day',SERVERROOT.'helpers',SERVERROOT.'hour',SERVERROOT.'languages',SERVERROOT.'libraries',SERVERROOT.'minute',SERVERROOT.'models',SERVERROOT.'tools');
+//array_push($arrCacheDir,SERVERROOT.'daemon',SERVERROOT.'day',SERVERROOT.'helpers',SERVERROOT.'hour',SERVERROOT.'languages',SERVERROOT.'libraries',SERVERROOT.'minute',SERVERROOT.'models',SERVERROOT.'tools');
 array_push($arrCacheDir,SERVERROOT.'helpers',SERVERROOT.'languages',SERVERROOT.'libraries',SERVERROOT.'models');
 $arrCacheFile = $arrCacheFileFailed = array();
+
+
+
+
+
+
 $arrDropTrue = array();
+/*
+//$arrDrop = array('controllers/RoleKnight.php', 'controllers/RoleWish.php', 'controllers/tools/Test.php', 'controllers/RoleInfo.php', 'controllers/RolePet.php', 'controllers/RoleExplore.php', 'controllers/RoleVilla.php', 'controllers/RoleInn.php', 'models/MRoleArena.php', 'models/MRoleChapter.php', 'models/MRoleCardItems.php', 'models/MRoleExplore.php', 'models/MRoleItems.php','helpers/post.php','libraries/ApiLib.php','libraries/QRedis.php','libraries/AMFSerializer.php');
+$arrDropTrue = array();
+foreach ($arrDrop as $key => $value)
+{
+    $arrDropTrue[$key] = PROJECTROOT.$value;
+}
+*/
 
 /* bin 目的目录检测*/
 if (!is_dir(APCBIN))
@@ -41,16 +56,15 @@ foreach ($arrCacheDir as $value)
 {
     compileDir($value);
 }
-
 apc_clear_cache ('user'); //清空导出之前的缓存
 /* 生成bin文件*/
  if (!writeBin())
 {
-    exit('write to '.APCBIN.'xyws_vn.bin failed!!!');
+    exit('write to '.APCBIN.BINNAME.' failed!!!');
 }
-$strBinMd5 = md5_file(APCBIN.'xyws_vn.bin');
-
+$strBinMd5 = md5_file(APCBIN.BINNAME);
 /* 未被缓存的php警告*/
+
 foreach ($arrCacheFileFailed as $value)
 {
     echo $value," cant't to cached .....";
@@ -124,20 +138,12 @@ function writeBin ()
     global $arrCacheFile, $arrDropTrue ;
     foreach ($arrCacheFile as $key => $value)
     {
-//        if (!in_array($value,$arrDropTrue))
-//        {
-//            echo $value,"--",$key,"<br />";
-//            if (! apc_bin_dumpfile(array($value), null, APCBIN.'xyws_vn.bin'.$key))
-//            {
-//                echo $value,"\n";
-//            }
-//        }
         if (in_array($value,$arrDropTrue))
         {
             unset($arrCacheFile[$key]);
         }
     }
-    return apc_bin_dumpfile($arrCacheFile, array(), APCBIN.'xyws_vn.bin');
+    return apc_bin_dumpfile($arrCacheFile, array(), APCBIN.BINNAME);
 }
 
 
@@ -147,7 +153,7 @@ function replaceFile ($strBinMd5)
     $strTime = date('Y-m-d H:i:s');
     foreach ($arrCacheFile as $value)
     {
-        if (!file_put_contents($value,"<?php\n/* \nThere is nothing,But do not be surprised,It's worked!\nCreated by JoyWay W1 Team (cfc4n@cnxct.com) at {$strTime}.\nxyws_vn.bin md5 : [{$strBinMd5}].\n*/\n?>"))
+        if (!file_put_contents($value,"<?php\n/* \nThere is nothing,But do not be surprised,It's worked!\nCreated by JoyWay W1 Team (cfc4n@cnxct.com) at {$strTime}.\nThis file md5 : [".md5_file($value)."]\n".BINNAME." md5  : [{$strBinMd5}].\n*/\n?>"))
         {
             echo 'file can\'t to clear empty'.$value;
             return false;
